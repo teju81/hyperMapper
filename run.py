a9dc74f05fda8e541b4e407e50c0a053c7c1f2ff
krankit00 <kr.ankit00@gmail.com>
2025-04-06 23:20:54 +0530

    GSPL Integration in Progress

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import pathlib
 from sfm_project.pycolmap_sfm.sfm_colmap import run_sfm 
 from sfm_project.pycolmap_sfm.utils import extract_frames, process_pointcloud
 import argparse
-from gsplat_light.internal.entrypoints.gspl import cli as gspl_pipeline
+# from gsplat_light.internal.entrypoints.gspl import cli as gspl_pipeline
 
 
 def sfm_pipeline():
@@ -18,14 +18,13 @@ def sfm_pipeline():
     image_dir = pathlib.Path(args.image_dir)
     output_path = pathlib.Path(args.output_path)
     extract_frames(args.video_path, image_dir, fps=args.fps)
-    maps = run_sfm(image_dir=image_dir, output_path=output_path, is_dense=False)
-    process_pointcloud(output_path, view_pcd=True, save_pcd=True, filter_pcd=True, is_dense=False)
+    maps = run_sfm(image_dir=image_dir, output_path=output_path, is_dense=True)
+    process_pointcloud(output_path, view_pcd=True, save_pcd=True, filter_pcd=True, is_dense=True)
 
 
 def main():
     sfm_pipeline()
-    
-    gspl_pipeline()
+    # gspl_pipeline()
 
 if __name__ == "__main__":
     main()
